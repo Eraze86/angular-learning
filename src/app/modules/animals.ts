007adef461859543c29168e7f4e933c73ab8ea4c
Eraze86 <therese.rosvall@hotmail.se>
2022-02-16 18:19:27 +0100

    crested a class

---
+++ src/app/modules/animals.ts
@@ -0,0 +1,13 @@
+export class Animals{
+    name: string;
+    age: number;
+    color: string;
+    isFed: boolean;
+
+    constructor(name: string; age: number; color: string){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.isFed = false;
+    }
+}
